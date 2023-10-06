ac25c11ad5c7cf6a9703e7210a1d52f8d447f601
karan-mandhare <2002karanmandhare@gmail.com>
2023-10-06 20:46:02 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [color, setColor] = useState("black");
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-48 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 
+          rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+
+          <button
+          onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 
+          rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+
+          <button
+          onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 
+          rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+
+          <button
+          onClick={() => setColor("yellow")}
+            className="outline-none px-4 py-1 
+          rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "yellow" }}
+          >
+            Yellow
+          </button>
+
+          <button
+          onClick={() => setColor("pink")}
+            className="outline-none px-4 py-1 
+          rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
